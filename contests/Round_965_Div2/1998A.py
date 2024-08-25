d8920f03c79b9cdc6d50ac040230e6f74cbae22e
Zenith1009 <naishadh.rana10@gmail.com>
2024-08-26 01:03:38 +0530

    Updated ststs

--- contests/Round_965_Div2/1998A.py
+++ contests/Round_965_Div2/1998A.py
@@ -1,14 +1,31 @@
-def solve():
-    t = int(input("Enter the number of test cases: "))
-    for i in range(t):
-        xc, yc, k = map(int, input("Enter xc yc k: ").split())
-        points = []
-        for j in range(k):
-            xi = xc + j
-            yi = yc - j
-            points.append((xi, yi))
+# def solve():
+#     t = int(input("Enter the number of test cases: "))
+#     for i in range(t):
+#         xc, yc, k = map(int, input("Enter xc yc k: ").split())
+#         points = []
+#         for j in range(k):
+#             xi = xc + j
+#             yi = yc - j
+#             points.append((xi, yi))
         
-        for point in points:
-            print(point[0], point[1])
+#         for point in points:
+#             print(point[0], point[1])
 
-solve()
+# solve()
+
+
+def solve():
+    x, y, k = map(int, input().split())
+    for i in range(k // 2):
+        print(x - i - 1, y)
+        print(x + i + 1, y)
+    if k % 2 != 0:
+        print(x, y)
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        solve()
+
+if __name__ == "__main__":
+    main()
